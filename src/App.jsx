d20da75630f916da449213eacab00780e4a0dbbe
Amishakul <awadhutkavita@gmail.com>
2025-05-03 16:34:28 +0530

    using tailwind v3

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import './index.css';
 function App() {
   return (
   <>
-    <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Tailwind CSS is working! 🎉</h1>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
     </>
   )
   
