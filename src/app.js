79338782572fb8dfdac769d1116eff1634d4e396
Amishakul <awadhutkavita@gmail.com>
2026-08-06 22:49:55 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -17,29 +17,96 @@ const app = express();
 //     res.send("Hello hello hello!")
 // });
 
-app.use("/user", (req, res) => {
-    res.send("HAHAHAHAHAAH")
-}); // Because of this route other remanining routes below will never get a chance to execute
+// app.use("/user", (req, res) => {
+//     res.send("HAHAHAHAHAAH")
+// }); // Because of this route other remanining routes below will never get a chance to execute
 
 // This will only handle GET calls to /user
-app.get("/user", (req, res) => {
+
+// this req can match /user, /user/xyz, /user/1
+// app.get("/user", (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+
+// this route will work for /abc, /ac
+// app.get("/ab?c", (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+
+// });
+
+ // can write as many b character as we want
+// app.get("/ab+c", (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+
+// });
+
+
+// start should be from ab and end should be with cd, in between we can write anything
+// app.get("/ab*cd", (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+
+// bc is optional
+// app.get("/a(bc)?d", (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+// can write as much of bc
+// app.get("/a(bc)+d", (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+
+//if there is "a" anywhere in the path it will work
+// app.get(/a/, (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+// it can start with anything but must end with fly word. eg. dragonfly, butterfly
+// app.get(/.*fly$/, (req, res) => {
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+
+// how to read query parameter in the route handler: http://localhost:7777/user?userid=101&password=testing
+// app.get("/user", (req, res) => {
+//     console.log(req.query)
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// })
+
+
+// http://localhost:7777/user/123
+
+// : means dynamic
+
+// app.get("/user/:userId", (req, res) => {
+//     console.log(req.params);
+//     res.send({firstname: "Amisha", lastname: "Kulkarni"})
+// });
+
+// http://localhost:7777/user/123/amisha/testing
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
     res.send({firstname: "Amisha", lastname: "Kulkarni"})
-})
-
-app.post("/user", (req, res) => {
-    // saving data to DB
-    res.send("Data sucessfully saved to the database!")
 
 })
 
-app.delete("/user", (req, res) => {
-    res.send("Deleted sucessfully!")
-})
+// app.post("/user", (req, res) => {
+//     // saving data to DB
+//     res.send("Data sucessfully saved to the database!")
+
+// })
+
+// app.delete("/user", (req, res) => {
+//     res.send("Deleted sucessfully!")
+// })
 
 // this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("Hello from the server!")
-});
+// app.use("/test", (req, res) => {
+//     res.send("Hello from the server!")
+// });
 
 // app.use("/", (req, res) => {
 //     res.send("Hello from the dashboard!")
