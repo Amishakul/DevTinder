bb30704035317356f831a4d24a70da686b03c346
Amishakul <awadhutkavita@gmail.com>
2026-08-06 14:59:29 +0530

    exploring  get, post, delete

--- src/app.js
+++ src/app.js
@@ -6,19 +6,44 @@ const express = require("express");
 const app = express();
 
 
-// request handler function
+// request handler function, these all are routes
 
-app.use("/hello", (req, res) => {
-    res.send("Hello hello hello!")
-});
+// app.use("/hello/2", (req, res) => {
+//     res.send("Abracadarab!")
+// });
 
+
+// app.use("/hello", (req, res) => {
+//     res.send("Hello hello hello!")
+// });
+
+app.use("/user", (req, res) => {
+    res.send("HAHAHAHAHAAH")
+}); // Because of this route other remanining routes below will never get a chance to execute
+
+// This will only handle GET calls to /user
+app.get("/user", (req, res) => {
+    res.send({firstname: "Amisha", lastname: "Kulkarni"})
+})
+
+app.post("/user", (req, res) => {
+    // saving data to DB
+    res.send("Data sucessfully saved to the database!")
+
+})
+
+app.delete("/user", (req, res) => {
+    res.send("Deleted sucessfully!")
+})
+
+// this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
     res.send("Hello from the server!")
 });
 
-app.use("/", (req, res) => {
-    res.send("Hello from the dashboard!")
-});
+// app.use("/", (req, res) => {
+//     res.send("Hello from the dashboard!")
+// });
 
 app.listen(7777, () => {
     console.log("Server is successfully listening on port 7777...")
